6bc056533676897dcf750ae1326121986f833610
Rutilio <rutilio.iraheta@gmail.com>
2021-04-08 21:36:31 -0600

    Add watcher and computed fields

--- 02 Basics/basics-03-events-starting-code/app.js
+++ 02 Basics/basics-03-events-starting-code/app.js
@@ -4,11 +4,43 @@ const app = Vue.createApp({
       counter: 0,
       name: "",
       confirmedName: "",
+      // fullname:""
+      lastName:""
     };
   },
+  watch:{
+    counter(value){
+      if (value>50){
+        this.counter=0;
+      }
+    }
+
+    /* name(value){
+      if (value===''){
+        this.fullname='';
+      }else{
+        this.fullname = value + ' Iraheta'
+      }
+    } */
+  },
+  computed: {
+    fullname() {
+      console.log('Here we go...');
+      if (this.name === "" && this.lastName==="") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + " " + lastName;
+    outputFullName() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Iraheta";
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter += num;
@@ -20,9 +52,13 @@ const app = Vue.createApp({
       alert("Submitted");
     },
     confirmInput() {
-      this.confirmedName = this.name
+      this.confirmedName = this.name;
     },
-  },
+    resetInput() {
+      this.name = "";
+      this.lastName="";
+    }
+  }
 });
 
 app.mount("#events");
