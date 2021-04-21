affe1be3ac1afb6ed9ff020c8c016c6f5436b3e7
Rutilio <rutilio.iraheta@gmail.com>
2021-04-21 17:18:44 -0600

    Removing list items

--- 03 Lists & Conditionals/lists-cond-01-starting-setup/app.js
+++ 03 Lists & Conditionals/lists-cond-01-starting-setup/app.js
@@ -1,12 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { enteredGoalValue:"", goals: [] };
+    return { enteredGoalValue: "", goals: [] };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(idx){
+      this.goals.splice(idx, 1);
     }
-  }
+  },
 });
 
 app.mount("#user-goals");
