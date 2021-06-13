240406d6a76c0411fa873d914010e1d136ec3396
Rutilio <rutilio.iraheta@gmail.com>
2021-06-12 22:28:31 -0600

    Adding a modal dialog

--- 10 Prj - Learning Resources app/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ 10 Prj - Learning Resources app/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid input" @close="confirmError">
+    <template #default>
+      <p>Error</p>
+      <p>Please check</p>
+    </template>
+    <template #actions>
+        <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -27,13 +36,28 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
-
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+    confirmError(){
+        this.inputIsInvalid=false;
     }
   }
 };
