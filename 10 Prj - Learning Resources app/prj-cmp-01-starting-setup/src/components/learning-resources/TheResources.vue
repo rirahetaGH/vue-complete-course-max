fcb04343c5dcd5aa88917270cf61b585fac03593
Rutilio <rutilio.iraheta@gmail.com>
2021-06-12 22:08:52 -0600

    Fetching user input

--- 10 Prj - Learning Resources app/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ 10 Prj - Learning Resources app/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,13 +1,19 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode" 
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')" :mode="addResourceButtonMode"  
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResourceButtonMode"
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -38,22 +44,33 @@ export default {
       ]
     };
   },
-  provide(){
+  provide() {
     return {
-        resources: this.storedResources
-    }  
+      resources: this.storedResources,
+      addResource: this.addResource
+    };
   },
   computed: {
-      storedResButtonMode(){
-          return this.selectedTab==='stored-resources' ? null: 'flat';
-      },
-      addResourceButtonMode(){
-          return this.selectedTab==='add-resource' ? null: 'flat';
-      }
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResourceButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    }
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     }
   }
 };
