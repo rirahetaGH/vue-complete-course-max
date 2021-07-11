f534ae078637174321e61013a4b0ab74a3ed9e49
Rutilio <rutilio.iraheta@gmail.com>
2021-07-10 22:01:09 -0600

    Loading data

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,10 +30,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading=true;
       fetch(
         "https://vue-http-demo-5bea6-default-rtdb.firebaseio.com/surveys.json"
       )
@@ -42,6 +45,7 @@ export default {
           }
         })
         .then(data => {
+          this.isLoading=false;
           const result = [];
           for (const id in data) {
             result.push({
@@ -54,6 +58,9 @@ export default {
         });
     },
   },
+  mounted(){
+    this.loadExperiences();
+  }
 };
 </script>
 
