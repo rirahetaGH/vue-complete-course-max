1f18bce336ce79a02501933cfd5e3ed2a3a5a6bc
Rutilio <rutilio.iraheta@gmail.com>
2021-07-18 10:55:35 -0600

    Routing setup

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, craeteWebHistory } from 'vue-router'
 
 import App from './App.vue';
+
+const router = createRouter({
+    history: craeteWebHistory(),
+    routes: []
+});
 
 const app = createApp(App)
 
