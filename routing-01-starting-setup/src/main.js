3dd40b3c93f7ac35baa9b0ea0c30d4dc5861696e
Rutilio <rutilio.iraheta@gmail.com>
2021-07-18 11:25:27 -0600

    Registering and rendering routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,13 +1,20 @@
 import { createApp } from 'vue';
-import { createRouter, craeteWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamList from './components/teams/TeamsList.vue';
+import UserList from './components/users/UsersList.vue';
 
 const router = createRouter({
-    history: craeteWebHistory(),
-    routes: []
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamList }, //our-domain.com/teams
+    { path: '/users', component: UserList } //our-domain.com/users
+  ]
 });
 
-const app = createApp(App)
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
