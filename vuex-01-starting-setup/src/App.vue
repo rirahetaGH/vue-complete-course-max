2e80d97f4b2fd315e2f452cd8cf590d93a4fec70
Rutilio <rutilio.iraheta@gmail.com>
2021-08-16 19:27:53 -0600

    vuex initial commit

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>{{ $store.state.counter }}</h3>
+  <button>Add 1</button>
+  <base-container title="Vuex"></base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
