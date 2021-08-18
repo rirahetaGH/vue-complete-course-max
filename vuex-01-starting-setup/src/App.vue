dfdeb05badc709dec961b7029621ac76ea727821
Rutilio <rutilio.iraheta@gmail.com>
2021-08-18 09:21:44 -0600

    Introducing getters

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,19 +1,24 @@
 <template>
+  <base-container title="Vuex">
   <the-counter></the-counter>
+  <favorite-value></favorite-value>
   <button @click="addOne">Add 1</button>
   <change-counter></change-counter>
-  <base-container title="Vuex"></base-container>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+
 export default {
   components: {
     BaseContainer,
     TheCounter,
-    ChangeCounter
+    ChangeCounter,
+    FavoriteValue
   },
   methods: {
     addOne() {
